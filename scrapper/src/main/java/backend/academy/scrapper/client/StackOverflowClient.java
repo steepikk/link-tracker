07abd18e1316c8a92ee49c3e0ce04dd66c52ccef
Stepa <stepan_popov_2025@mail.ru>
2025-04-18 13:25:53 +0300

    feat: create new StackOverflowClient

--- scrapper/src/main/java/backend/academy/scrapper/client/StackOverflowClient.java
+++ scrapper/src/main/java/backend/academy/scrapper/client/StackOverflowClient.java
@@ -1,65 +1,136 @@
 package backend.academy.scrapper.client;
 
-import backend.academy.common.dto.StackOverflowContent;
+import backend.academy.common.dto.ContentUpdate;
+import backend.academy.common.dto.UpdateType;
 import backend.academy.scrapper.config.ScrapperConfig;
 import backend.academy.scrapper.exception.QuestionNotFoundException;
 import backend.academy.scrapper.util.LinkToApiRequestConverter;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientResponseException;
 
 @Component
+@Slf4j
 public class StackOverflowClient {
-    private final ScrapperConfig scrapperConfig;
     private final RestClient restClient;
     private final ObjectMapper objectMapper;
     private final LinkToApiRequestConverter linkToApiRequestConverter;
 
+    private final String key;
+    private final String access_token;
+    private final String filter;
+
+    private static final String JSON_ERROR = "Ошибка при разборе JSON-Ответа";
+    private static final String REQUEST_ERROR = "Ошибка при запросе";
+
     public StackOverflowClient(ScrapperConfig scrapperConfig, RestClient restClient, ObjectMapper objectMapper, LinkToApiRequestConverter linkToApiRequestConverter) {
-        this.scrapperConfig = scrapperConfig;
         this.restClient = restClient;
         this.objectMapper = objectMapper;
         this.linkToApiRequestConverter = linkToApiRequestConverter;
+
+        this.key = scrapperConfig.stackOverflow().key();
+        this.access_token = scrapperConfig.stackOverflow().accessToken();
+        this.filter = "withbody";
     }
 
-    public Instant getLastUpdated(String questionUrl) {
-        RestClient restClient = RestClient.create();
+    private String performRequest(String url) {
+        url += "&key=" + key + "&access_token=" + access_token + "&filter=" + filter;
 
-        String questionId = questionUrl.split("/")[4];
-        String apiUrl = "https://api.stackexchange.com/2.3/questions/" + questionId + "?site=stackoverflow" + "&key="
-                + scrapperConfig.stackOverflow().key() + "&access_token="
-                + scrapperConfig.stackOverflow().accessToken();
-
-        String response = "";
         try {
-            response = restClient.get().uri(apiUrl).retrieve().body(String.class);
+            return restClient.get().uri(url).retrieve().body(String.class);
         } catch (RestClientResponseException e) {
-            throw new QuestionNotFoundException("Не удалось найти вопрос по ссылке");
+            log.atError().addKeyValue("link", url).setMessage(REQUEST_ERROR).log();
+            throw new QuestionNotFoundException("Ошибка при запросе: " + url);
         }
+    }
+
+    private List<ContentUpdate> parseContentList(String jsonResponse, UpdateType type, String title, String url) {
+        List<ContentUpdate> stackOverflowContentList = new ArrayList<>();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+            jsonNode.get("items").forEach(item -> {
+                String ownerName = item.get("owner").get("display_name").asText();
+                String body = item.get("body").asText();
+                String creationDate = item.get("creation_date").asText();
+                Instant createdInstant = Instant.parse(creationDate);
+                stackOverflowContentList.add(new ContentUpdate(type, title, ownerName, createdInstant, body));
+            });
+        } catch (Exception e) {
+            log.atError().addKeyValue("link", url).setMessage(JSON_ERROR).log();
+            throw new HttpMessageNotReadableException("Ошибка при разборе JSON-ответа по URL " + url);
+        }
+        return stackOverflowContentList;
+    }
+
+    public String getTitle(String link) {
+        String url = linkToApiRequestConverter.convertSOtoQuestion(link);
+        String jsonResponse = performRequest(url);
+        try {
+            JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+            return jsonNode.get("items").get(0).get("title").asText();
+        } catch (HttpMessageNotReadableException | JsonProcessingException e) {
+            log.atError().addKeyValue("link", url).setMessage(JSON_ERROR).log();
+            throw new HttpMessageNotReadableException("Ошибка при разборе JSON-ответа по URL " + url);
+        } catch (Exception e) {
+            log.atError()
+                    .addKeyValue("link", url)
+                    .setMessage("Некорректное тело запроса")
+                    .log();
+            throw new RuntimeException("Некорректное тело запроса по URL " + url);
+        }
+    }
+
+    private List<ContentUpdate> getComments(String link, String title) {
+        String soUrl = linkToApiRequestConverter.convertSOtoQuestionComments(link);
+        return parseContentList(performRequest(soUrl), UpdateType.COMMENT, title, soUrl);
+    }
+
+    private List<ContentUpdate> getAnswers(String link, String title) {
+        String soUrl = linkToApiRequestConverter.convertSOtoQuestionAnswers(link);
+        return parseContentList(performRequest(soUrl), UpdateType.ANSWER, title, soUrl);
+    }
+
+    private List<ContentUpdate> getAnswerComments(String link, String title) {
+        List<ContentUpdate> stackOverflowContentList = new ArrayList<>();
+        String url = linkToApiRequestConverter.convertSOtoQuestionAnswers(link);
+        String jsonResponse = performRequest(url);
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonResponse = objectMapper.readTree(response);
-
-            JsonNode items = jsonResponse.get("items");
-            if (items != null && items.size() > 0) {
-                JsonNode firstItem = items.get(0);
-                if (firstItem.has("last_activity_date")) {
-                    long lastActivityDate = firstItem.get("last_activity_date").asLong();
-                    return Instant.ofEpochSecond(lastActivityDate);
-                }
-            }
+            JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+            jsonNode.get("items").forEach(item -> {
+                String answerId = item.get("answer_id").asText();
+                String commentsUrl = linkToApiRequestConverter.convertSOtoAnswerCommentsLink(answerId, link);
+                stackOverflowContentList.addAll(
+                        parseContentList(performRequest(commentsUrl), UpdateType.COMMENT, title, commentsUrl));
+            });
+        } catch (HttpMessageNotReadableException | JsonProcessingException e) {
+            log.atError().addKeyValue("link", url).setMessage(REQUEST_ERROR).log();
+            throw new HttpMessageNotReadableException(e.getMessage());
         } catch (Exception e) {
-            throw new QuestionNotFoundException("Не удалось обработать данные ответа");
+            log.atError().addKeyValue("link", url).setMessage(REQUEST_ERROR).log();
+            throw new QuestionNotFoundException(e.getMessage());
         }
 
-        return null;
+        return stackOverflowContentList;
+    }
+
+    public List<ContentUpdate> getSOContent(String link) {
+        String title = getTitle(link);
+
+        List<ContentUpdate> stackOverflowContentList = new ArrayList<>();
+        stackOverflowContentList.addAll(getComments(link, title));
+        stackOverflowContentList.addAll(getAnswers(link, title));
+        stackOverflowContentList.addAll(getAnswerComments(link, title));
+
+        return stackOverflowContentList;
     }
 }
--- scrapper/src/main/java/backend/academy/scrapper/util/LinkToApiRequestConverter.java
+++ scrapper/src/main/java/backend/academy/scrapper/util/LinkToApiRequestConverter.java
@@ -12,4 +12,39 @@ public class LinkToApiRequestConverter {
 
         return String.format("https://api.github.com/repos/%s/%s/issues", owner, repoName);
     }
+
+    public String convertSOtoQuestionComments(String link) {
+        String[] linkParts = link.split("/");
+
+        String site = linkParts[2];
+        String questionId = linkParts[4];
+
+        return String.format("https://api.stackexchange.com/2.3/questions/%s/comments?site=%s", questionId, site);
+    }
+
+    public String convertSOtoQuestion(String link) {
+        String[] linkParts = link.split("/");
+
+        String site = linkParts[2];
+        String questionId = linkParts[4];
+
+        return String.format("https://api.stackexchange.com/2.3/questions/%s?site=%s", questionId, site);
+    }
+
+    public String convertSOtoQuestionAnswers(String link) {
+        String[] linkParts = link.split("/");
+
+        String site = linkParts[2];
+        String questionId = linkParts[4];
+
+        return String.format("https://api.stackexchange.com/2.3/questions/%s/answers?site=%s", questionId, site);
+    }
+
+    public String convertSOtoAnswerCommentsLink(String answerId, String link) {
+        String[] linkParts = link.split("/");
+
+        String site = linkParts[2];
+
+        return String.format("https://api.stackexchange.com/2.3/answers/%s/comments?site=%s", answerId, site);
+    }
 }
