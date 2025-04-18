292e05a3037fce2d9b730f295ccf3842eecfc5b0
Stepa <stepan_popov_2025@mail.ru>
2025-04-18 13:26:11 +0300

    feat: integrate clients in scrapper

--- scrapper/src/main/java/backend/academy/scrapper/service/LinkMonitoringService.java
+++ scrapper/src/main/java/backend/academy/scrapper/service/LinkMonitoringService.java
@@ -1,5 +1,6 @@
 package backend.academy.scrapper.service;
 
+import backend.academy.common.dto.ContentUpdate;
 import backend.academy.common.dto.LinkUpdate;
 import backend.academy.scrapper.client.GitHubClient;
 import backend.academy.scrapper.client.StackOverflowClient;
@@ -9,6 +10,7 @@ import java.util.List;
 
 import backend.academy.scrapper.sender.NotificationSenderService;
 import backend.academy.scrapper.entity.Chat;
+import backend.academy.scrapper.service.link.LinkService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -30,56 +32,94 @@ public class LinkMonitoringService {
         this.stackOverflowClient = stackOverflowClient;
     }
 
-    /*@Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 60000)
     public void monitorLinks() {
         for (Link linkEntry : linkService.getAllLinks()) {
             if (linkEntry.url().contains("github.com")) {
                 checkGitHubLink(linkEntry);
-            } *//*else if (linkEntry.getUrl().contains("stackoverflow.com")) {
+            } else if (linkEntry.url().contains("stackoverflow.com")) {
                 checkStackOverflowLink(linkEntry);
-            }*//*
+            }
         }
-    }*/
+    }
 
     private void checkGitHubLink(Link linkEntry) {
         Instant lastUpdated = gitHubClient.getLastUpdated(linkEntry.url());
-        if (linkEntry.lastUpdated() == null && lastUpdated != null) {
-            linkEntry.lastUpdated(lastUpdated);
+        if (lastUpdated == null || (linkEntry.lastUpdated() != null && !lastUpdated.isAfter(linkEntry.lastUpdated()))) {
             return;
         }
-        if (lastUpdated != null && !lastUpdated.equals(linkEntry.lastUpdated())) {
-            linkEntry.lastUpdated(lastUpdated);
-            linkService.updateLink(linkEntry);
 
-            List<Long> chatIds = linkEntry.chats()
-                    .stream()
-                    .map(Chat::chatId)
-                    .toList();
+        List<ContentUpdate> contents = gitHubClient.getGitHubContent(linkEntry.url());
+        if (contents.isEmpty()) {
+            return;
+        }
 
+        List<ContentUpdate> newUpdates = contents.stream()
+                .filter(content -> linkEntry.lastUpdated() == null || content.createdAt().isAfter(linkEntry.lastUpdated()))
+                .toList();
+
+        if (newUpdates.isEmpty()) {
+            return;
+        }
+
+        linkEntry.lastUpdated(lastUpdated);
+        linkService.updateLink(linkEntry);
+
+        List<Long> chatIds = linkEntry.chats()
+                .stream()
+                .map(Chat::chatId)
+                .toList();
+
+        for (ContentUpdate update : newUpdates) {
             LinkUpdate linkUpdate = new LinkUpdate(
                     linkEntry.id(),
                     linkEntry.url(),
-                    "GitHub repository updated",
+                    update,
                     chatIds
             );
-
             notificationSenderService.sendNotification(linkUpdate).subscribe();
         }
     }
 
-    /*private void checkStackOverflowLink(Link linkEntry) {
-        Instant lastUpdated = stackOverflowClient.getLastUpdated(linkEntry.getUrl());
-        if (lastUpdated != null && !lastUpdated.equals(linkEntry.getLastUpdated())) {
-            linkEntry.setLastUpdated(lastUpdated);
-            linkRepository.updateLink(linkEntry);
+    private void checkStackOverflowLink(Link linkEntry) {
+        List<ContentUpdate> contents = stackOverflowClient.getSOContent(linkEntry.url());
+        if (contents.isEmpty()) {
+            return;
+        }
 
+        Instant lastContentUpdate = contents.stream()
+                .map(ContentUpdate::createdAt)
+                .max(Instant::compareTo)
+                .orElse(null);
+
+        if (lastContentUpdate == null || (linkEntry.lastUpdated() != null && !lastContentUpdate.isAfter(linkEntry.lastUpdated()))) {
+            return;
+        }
+
+        List<ContentUpdate> newUpdates = contents.stream()
+                .filter(content -> linkEntry.lastUpdated() == null || content.createdAt().isAfter(linkEntry.lastUpdated()))
+                .toList();
+
+        if (newUpdates.isEmpty()) {
+            return;
+        }
+
+        linkEntry.lastUpdated(lastContentUpdate);
+        linkService.updateLink(linkEntry);
+
+        List<Long> chatIds = linkEntry.chats()
+                .stream()
+                .map(Chat::chatId)
+                .toList();
+
+        for (ContentUpdate update : newUpdates) {
             LinkUpdate linkUpdate = new LinkUpdate(
-                    linkEntry.getId(),
-                    linkEntry.getUrl(),
-                    "StackOverflow question updated",
-                    List.copyOf(linkEntry.getTgChatIds()));
-
+                    linkEntry.id(),
+                    linkEntry.url(),
+                    update,
+                    chatIds
+            );
             notificationSenderService.sendNotification(linkUpdate).subscribe();
         }
-    }*/
+    }
 }
